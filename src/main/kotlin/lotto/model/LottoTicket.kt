a5247a920203f55f2031a6add003a2abf701a829
hyun <sarahs19991115@gmail.com>
2024-03-11 23:18:56 +0900

    feat(lottoTicket): 로또 범위 1~45 확인 기능
     docs(readMe): 구현한 기능 표시

--- src/main/kotlin/lotto/model/LottoTicket.kt
+++ src/main/kotlin/lotto/model/LottoTicket.kt
@@ -2,7 +2,7 @@ package lotto.model
 
 class LottoTicket() {
     fun makeLottoTicket(): List<Int> {
-        val lottoRange = 1..100
+        val lottoRange = 1..45
         val lottoTicket = lottoRange.shuffled().chunked(6)[0]
         return lottoTicket
     }
--- src/test/kotlin/lottoTest/modelTest/LottoTicketTest.kt
+++ src/test/kotlin/lottoTest/modelTest/LottoTicketTest.kt
@@ -1,8 +1,10 @@
 package lottoTest.modelTest
 
+import io.kotest.data.forAll
 import lotto.model.LottoTicket
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class LottoTicketTest {
     @Test
@@ -16,5 +18,17 @@ class LottoTicketTest {
 
         //then
         assertThat(lottoTicket.count()).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 번호의 숫자 범위는 1~45까지이다`() {
+        //given
+        val lotto = LottoTicket()
+        val lottoTicket = lotto.makeLottoTicket()
+
+        //when
+
+        //then
+        assertThat(lottoTicket).allMatch{ it in 1..45 }
     }
 }
