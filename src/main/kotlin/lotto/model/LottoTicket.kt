225c8005c9f66221a85b95c9ffdc9f35d6a4a342
hyun <sarahs19991115@gmail.com>
2024-03-13 18:34:18 +0900

    refactor(lottoTicket): 일급컬렉션으로써 변수가 하나라는 걸 명확히 하기 위해 변수 선언과 할당 부분 분리

--- src/main/kotlin/lotto/model/LottoTicket.kt
+++ src/main/kotlin/lotto/model/LottoTicket.kt
@@ -3,8 +3,9 @@ package lotto.model
 private val NUMBER_AMOUNT = 6
 private val NUMBER_RANGE = 1..45
 class LottoTicket() {
+    private lateinit var lottoTicket: List<Int>
     fun makeLottoTicket(): List<Int> {
-        val lottoTicket = NUMBER_RANGE.shuffled().chunked(NUMBER_AMOUNT)[0]
+        lottoTicket = NUMBER_RANGE.shuffled().chunked(NUMBER_AMOUNT)[0]
         return lottoTicket.sorted()
     }
 }
