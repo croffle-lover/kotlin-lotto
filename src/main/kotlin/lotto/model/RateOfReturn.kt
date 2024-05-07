a85b56acced7c147859aea537e2cf79d637eb2eb
hyun <sarahs19991115@gmail.com>
2024-05-07 13:30:24 +0900

    refactor($RateOfReturn): 반올림 기능 함수 round로 정정

--- src/main/kotlin/lotto/model/RateOfReturn.kt
+++ src/main/kotlin/lotto/model/RateOfReturn.kt
@@ -1,10 +1,10 @@
 package lotto.model
 
-import kotlin.math.floor
+import kotlin.math.round
 
 class RateOfReturn {
     fun getRateOfReturn(money: Int, winningMoney: Int): Double {
         val rate = winningMoney.toDouble() / money.toDouble()
-        return floor(rate * 100) / 100
+        return round(rate * 100) / 100
     }
 }
